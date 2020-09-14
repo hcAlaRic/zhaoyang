b8215a49e51d3811a94ed78a5de48f70dd2e5878
hcAlaRic <2431019089@qq.com>
2020-09-15 01:01:33 +0800

    first commit

---
+++ test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("success"))
+	})
+	http.ListenAndServe("127.0.0.1:8080", nil)
+}
